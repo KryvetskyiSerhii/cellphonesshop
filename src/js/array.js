4f1b3293fa23c7414d76d4e1ca8711275d9be52d
Kryvetskyi Serhii <sam.hartman@mail.ru>
2022-01-27 12:46:49 +0800

    second

---
+++ src/js/array.js
@@ -0,0 +1,75 @@
+let itemsArray = [
+    {
+    name: 'Alcatel GO Flip',
+    price: 96,
+    content: `<div class="phones__item item-phones">
+    <div class="item-phones__wrapper">
+        <div class="item-phones__image">
+            <img src="img/phones/alcatelgo.jpg" alt="alactel-go">
+        </div>
+        <div class="item-phones__content">
+            <div class="item-phones__name">Alcatel GO FLIP</div>
+            <div class="item-phones__price">96$</div>
+        </div>
+        <div class="item-phones__cart">
+            <img src="img/bag.png" alt="bag">
+        </div>
+    </div>
+</div>`
+    },
+    {
+        name: 'Cat S62',
+        price: 498,
+        content: `<div class="phones__item item-phones">
+        <div class="item-phones__wrapper">
+            <div class="item-phones__image">
+                <img src="img/phones/cats62.jpg" alt="cats62">
+            </div>
+            <div class="item-phones__content">
+                <div class="item-phones__name">Cat S62</div>
+                <div class="item-phones__price">498$</div>
+            </div>
+            <div class="item-phones__cart">
+                <img src="img/bag.png" alt="bag">
+            </div>
+        </div>
+    </div>`
+    },
+    {
+        name: 'One Plus NORD N200 5G',
+        price: 216,
+        content: `<div class="phones__item item-phones">
+        <div class="item-phones__wrapper">
+            <div class="item-phones__image">
+                <img src="img/phones/OnePlusnordn200.jpg" alt="oneplusnord200">
+            </div>
+            <div class="item-phones__content">
+                <div class="item-phones__name">One Plus NORD N200 5G</div>
+                <div class="item-phones__price">216$</div>
+            </div>
+            <div class="item-phones__cart">
+                <img src="img/bag.png" alt="bag">
+            </div>
+        </div>
+    </div>`
+    },
+    {
+        name: 'Samsung Galaxy A03s',
+        price: 168,
+        content: `<div class="phones__item item-phones">
+        <div class="item-phones__wrapper">
+            <div class="item-phones__image">
+                <img src="img/phones/galaxyA03s.jpg" alt="galaxyA30s">
+            </div>
+            <div class="item-phones__content">
+                <div class="item-phones__name">Samsung Galaxy A03s</div>
+                <div class="item-phones__price">168$</div>
+            </div>
+            <div class="item-phones__cart">
+                <img src="img/bag.png" alt="bag">
+            </div>
+        </div>
+    </div>`
+    }
+   
+]
